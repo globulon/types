6865a3d14cb7b201bde2224f2bbae16991541bdd
Marc-Daniel Ortega <globulon@gmail.com>
2017-11-13 01:54:30 +0100

    [IMP] demo version improved

--- src/main/scala/com/persgroep/types/Schema.scala
+++ src/main/scala/com/persgroep/types/Schema.scala
@@ -1,12 +1,12 @@
 package com.persgroep.types
 
-import scalaz._
 import scala.language.{higherKinds, implicitConversions, postfixOps}
 import scalaz.syntax.std.boolean._
+import scalaz.{Free, _}
 
 trait PropertyName[A]
-case class Property[P[_] <: PropertyName[_], O, A](n: P[A], get: O ⇒ A)
 //The free applicative describes the props as a continuous description of objects deductible
+case class Property[P[_] <: PropertyName[_], O, A](n: P[A], get: O ⇒ A)
 case class ObjectMeta[P[_] <: PropertyName[_], A](props: Free[({type λ[α] = Property[P, A, α]})#λ, A])
 
 trait Properties {
@@ -15,23 +15,34 @@ trait Properties {
 }
 
 trait Persons { self: Properties ⇒
+  protected type PersonProp[A] = Property[PersonMeta, Person, A]
+  protected type MakeChange[A] = A ⇒ Change[A]
+  protected type Comparison[A] = Property[PersonMeta, Person, A]
+  protected type StoreChange[A] = State[List[Change[_]], A]
 
-  final protected def propsToDif(src: Person, tgt: Person) =
-    new (({type λ[α] = Property[PersonMeta, Person, α]})#λ ~> ({type λ[α] = State[List[Change], α]})#λ) {
-      override def apply[A](fa: Property[PersonMeta, Person, A]): State[List[Change], A] = fa match {
-        case Property(PersonName(), get) ⇒ State[List[Change], String] {
-          diffs ⇒ (diffs ++ (get(src) != get(tgt)).option(get(tgt)).map(NewName(_)).toList, get(tgt))
-        }
-        case Property(PersonAge(), get) ⇒ State[List[Change], Int] {
-          diffs ⇒ (diffs ++ (get(src) != get(tgt)).option(get(tgt)).map(NewAge(_)).toList, get(tgt))
+  protected def personName: Comparison[String] = Property[PersonMeta, Person, String](PersonName(), _.name)
+  protected def personAge: Comparison[Int]  = Property[PersonMeta, Person, Int](PersonAge(), _.age)
+
+
+  final protected def propToChange = new (PersonMeta ~> MakeChange) {
+    override def apply[A](fa: PersonMeta[A]) = fa match {
+      case PersonName() ⇒ NewName(_)
+      case PersonAge()  ⇒ NewAge(_)
+    }
+  }
+
+  final protected def propsToChange(src: Person, tgt: Person) =
+    new (PersonProp ~> StoreChange) {
+      override def apply[A](fa: PersonProp[A]): StoreChange[A] = fa match {
+        case Property(name, get) ⇒ State[List[Change[_]], A] {
+          diffs ⇒ (diffs ++ (get(src) != get(tgt)).option(get(tgt)).map(propToChange(name)).toList, get(tgt))
         }
       }
     }
 
-  final protected def compare: Free[({type λ[α] = Property[PersonMeta, Person, α]})#λ, Unit] = for {
-    _ ← Property[PersonMeta, Person, String](PersonName(), _.name)
-    _ ← Property[PersonMeta, Person, Int](PersonAge(), _.age)
-  } yield ()
+  final protected def run(comparison: Free[({type λ[α] = Property[PersonMeta, Person, α]})#λ, Unit])(src: Person, tgt: Person) =
+    comparison.foldMap[({type λ[α] = State[List[Change[_]], α]})#λ](propsToChange(src, tgt)).run(List.empty)._1
+
 }
 
 case class Person(name: String, age: Int)
@@ -40,15 +51,20 @@ sealed trait PersonMeta[A] extends PropertyName[A]
 case class PersonName() extends PersonMeta[String]
 case class PersonAge() extends PersonMeta[Int]
 
-sealed trait Change
-case class NewName(v: String) extends Change
-case class NewAge(v: Int) extends Change
-
+sealed trait Change[A]
+case class NewName(v: String) extends Change[String]
+case class NewAge(v: Int) extends Change[Int]
 
 object Run extends Persons with Properties {
+  private def comparison = for {
+    _ ← personName
+    _ ← personAge
+  } yield ()
+
+
   def main(args: Array[String]): Unit = {
-    println(compare.foldMap[({type λ[α] = State[List[Change], α]})#λ](propsToDif(Person("alex", 33), Person("alex acevedo", 33))).run(List.empty)._1)
-    println(compare.foldMap[({type λ[α] = State[List[Change], α]})#λ](propsToDif(Person("alex", 33), Person("alex", 35))).run(List.empty)._1)
-    println(compare.foldMap[({type λ[α] = State[List[Change], α]})#λ](propsToDif(Person("alex", 33), Person("alex acevedo", 35))).run(List.empty)._1)
+    println(run(comparison)(Person("alex", 33), Person("alex acevedo", 33)))
+    println(run(comparison)(Person("alex", 33), Person("alex", 35)))
+    println(run(comparison)(Person("alex", 33), Person("alex acevedo", 35)))
   }
 }
